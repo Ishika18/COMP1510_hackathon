9b6b7807e6911d53d95f4663663bb5b4200a8be1
TerryLun <tianweilun@yahoo.com>
2020-04-17 10:44:58 -0700

    initial commit

--- nearest_store.py
+++ nearest_store.py
@@ -6,3 +6,54 @@ import itertools
 import webbrowser
 import folium
 import pandas
+
+
+def get_current_location() -> tuple:
+    """ user should enter postal code for regex"""
+    pass
+
+
+def find_closest_stores(current_latitude: float, current_longitude: float) -> list:
+    pass
+
+
+def get_populartimes(key: str, stores: list) -> list:
+    """ use set """
+    pass
+
+
+@decorator
+def rank_stores(stores: list):
+    pass
+
+
+def get_api_key() -> str:
+    return 'API KEY'
+
+
+def generate_map(stores) -> str:
+    pass
+
+
+def print_top_five_stores(stores):
+    pass
+
+
+def run():
+    key = get_api_key()
+    current_latitude, current_longitude = get_current_location()
+    stores = find_closest_stores(current_latitude, current_longitude)
+    stores = get_populartimes(key, stores)
+    rank_stores(stores)
+    print_top_five_stores(stores)
+    html_file_name = generate_map(stores)
+    webbrowser.open(html_file_name)
+
+
+def main():
+    doctest.testmod()
+    run()
+
+
+if __name__ == '__main__':
+    main()
