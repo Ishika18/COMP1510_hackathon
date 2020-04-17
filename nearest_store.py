452cd85f866fa90b132425506fc3ca985ec98da4
Ishika18 <shagunphw@gmail.com>
2020-04-17 11:56:34 -0700

    rank_stores created

--- nearest_store.py
+++ nearest_store.py
@@ -22,10 +22,21 @@ def get_populartimes(key: str, stores: list) -> list:
     pass
 
 
+def get_score(store: dict):
+    wait_time = store['wait_time']  # wait_time key will be appended with populartimes
+    distance = store['distance']
+    return wait_time / distance
+
+
 @decorator
 def rank_stores(stores: list):
-    # make a new dictionary with key values given by algorithm and value of the store
-    # for store in stores:
+    score_dict = {}
+    for store in stores:
+        score = get_score(store)
+        score_dict[score] = store
+    top_scores = sorted(score_dict)
+    # SYNTACTIC SUGAR
+    return [score_dict[top_scores[i]] for i in range(5)]
 
 
 def get_api_key() -> str:
@@ -54,7 +65,7 @@ def get_distance(key: str, stores: list, current_position: tuple) -> list:
             pass
         distance_json = json.loads(res.text)
         store_distance = distance_json['rows']['elements'][0]['distance']['value']  # distance in meters
-        store['store_distance'] = store_distance
+        store['distance'] = store_distance
     return stores
 
 
@@ -64,9 +75,9 @@ def run():
     stores = find_closest_stores(current_latitude, current_longitude)
     stores = get_populartimes(key, stores)  # ['popular_times'] ['wait_times']
     stores = get_distance(key, stores, (current_latitude, current_longitude))  # ['distance']
-    rank_stores(stores)
-    print_top_five_stores(stores)
-    html_file_name = generate_map(stores)
+    top_five_stores = rank_stores(stores)
+    print_top_five_stores(top_five_stores)
+    html_file_name = generate_map(top_five_stores)
     webbrowser.open(html_file_name)
 
 
