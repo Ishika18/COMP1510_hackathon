dd95545a4cead9e74a2489deb137500d4b86d7e7
ryanl <rleung1004@gmail.com>
2020-04-17 11:53:24 -0700

    implemented get_current_location() and get_coordinate_data()

--- nearest_store.py
+++ nearest_store.py
@@ -6,15 +6,61 @@ import itertools
 import webbrowser
 import folium
 import pandas
+import re
 
 
 def get_current_location() -> tuple:
     """ user should enter postal code for regex"""
-    pass
+    postal_code = prompt_postal_code()
+
+    key = get_api_key()
+
+    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address='
+                            f'{postal_code}&key={key}')
+    response.raise_for_status()
+    data = get_coordinate_data(postal_code)
+    lat = data['results'][0]['geometry']['location']['lat']
+    lon = data['results'][0]['geometry']['location']['lng']
+    return lat, lon
 
 
-def find_closest_stores(current_latitude: float, current_longitude: float) -> list:
-    pass
+def get_coordinate_data(postal_code: str) -> dict:
+    key = get_api_key()
+    response = requests.get(f'https://maps.googleapis.com/maps/api/geocode/json?address='
+                            f'{postal_code}&key={key}')
+    response.raise_for_status()
+    data = json.loads(response.text)
+    if data['status'] == 'ZERO_RESULTS':
+        raise ValueError("This location does not exist.")
+    else:
+        return data
+
+
+def prompt_postal_code() -> str:
+    postal_code = input('Enter a Canadian postal code in the format \'A1A 1A1\': ')
+    pattern = re.compile(r'^[A-Z]\d[A-Z] ?\d[A-Z]\d$')
+    if pattern.search(postal_code):
+        return postal_code
+    else:
+        raise ValueError("Please enter a valid Canadian postal code.")
+
+
+def find_closest_stores(current_latitude: float, current_longitude: float) -> dict:
+    payload = {'location': f'{current_latitude},{current_longitude}',
+               'rankby': 'distance',
+               'type': 'grocery_or_supermarket',
+               'key': get_api_key()}
+    data = get_store_results(payload)
+
+    return data
+
+
+def get_store_results(payload) -> dict:
+    response = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?', params=payload)
+    response.raise_for_status()
+    data = json.loads(response.text)
+
+    return data['results']
 
 
 def get_populartimes(key: str, stores: list) -> list:
@@ -22,13 +68,12 @@ def get_populartimes(key: str, stores: list) -> list:
     pass
 
 
-@decorator
 def rank_stores(stores: list):
     pass
 
 
 def get_api_key() -> str:
-    return 'API KEY'
+    return 'AIzaSyAVHKBzu0YpNi3o6Y_nYSY_wzNoDTN51mQ'
 
 
 def generate_map(stores) -> str:
@@ -44,15 +89,20 @@ def get_distance(key, stores) -> list:
 
 
 def run():
-    key = get_api_key()
-    current_latitude, current_longitude = get_current_location()
+    while True:
+        try:
+            current_latitude, current_longitude = get_current_location()
+        except ValueError as error_message:
+            print(error_message)
+        else:
+            break
     stores = find_closest_stores(current_latitude, current_longitude)
-    stores = get_populartimes(key, stores)  # ['popular_times'] ['wait_times']
-    stores = get_distance(key, stores)  # ['distance']
-    rank_stores(stores)
-    print_top_five_stores(stores)
-    html_file_name = generate_map(stores)
-    webbrowser.open(html_file_name)
+    # stores = get_populartimes(key, stores)  # ['popular_times'] ['wait_times']
+    # stores = get_distance(key, stores)  # ['distance']
+    # rank_stores(stores)
+    # print_top_five_stores(stores)
+    # html_file_name = generate_map(stores)
+    # webbrowser.open(html_file_name)
 
 
 def main():
